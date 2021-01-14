f5f55c41f0a16635a12f38022169180fa2c67bdf
kazuyanakata <68310528+kazuyanakata@users.noreply.github.com>
2021-01-14 15:47:54 +0900

    fix:ファイル修正
    ・不要な実装を削除
    ・アクション名変更
    ・コメント追加
    ・見た目の変更

--- html/mycakeapp/src/Controller/MainController.php
+++ html/mycakeapp/src/Controller/MainController.php
@@ -4,8 +4,6 @@ namespace App\Controller;
 
 use App\Controller\AppController;
 use Cake\I18n\Time;
-use Cake\Event\Event;
-use Exception;
 
 class MainController extends AppController
 {
@@ -19,25 +17,27 @@ class MainController extends AppController
     $this->loadModel('Discounts');
   }
 
-  public function schedule()
+  public function movieSchedule()
   {
     $week = ['日', '月', '火', '水', '木', '金', '土'];
     $today = Time::now();
+    //指定した日(今は$today)が映画の上映期間内で、かつ指定した日の上映スケジュールを抽出
+    $movies = $this->Movies->find()
+      ->where(['started_at <=' => $today])
+      ->andWhere(['finished_at >=' => $today])
+      ->andWhere(['is_deleted' => 0])
+      ->order(['started_at' => 'desc'])
+      ->contain('Schedules', function ($q) use ($today) {
+        return $q->where(['start_date >=' => $today->format('Y-m-d 00:00:00')])
+          ->andWhere(['start_date <=' => $today->format('Y-m-d 23:59:59')])
+          ->andWhere(['is_deleted' => 0])
+          ->order(['start_date' => 'asc']);
+      });
+    //指定した日(今は$today)から7日の日時を配列に代入
     for ($i = 0; $i < 7; $i++) {
       $date = Time::now();
       $dates[$i] = $date->addDays($i);
     }
-    $movies = $this->Movies->find()
-      ->where(['Movies.started_at <=' => $today])
-      ->andWhere(['Movies.finished_at >=' => $today])
-      ->andWhere(['Movies.is_deleted' => 0])
-      ->order(['Movies.started_at' => 'desc'])
-      ->contain('Schedules', function ($q) use ($today) {
-        return $q->where(['Schedules.start_date >=' => $today->format('Y-m-d 00:00:00')])
-          ->andWhere(['Schedules.start_date <=' => $today->format('Y-m-d 23:59:59')])
-          ->andWhere(['Schedules.is_deleted' => 0])
-          ->order(['Schedules.start_date' => 'asc']);
-      });
     $this->set(compact('dates', 'week', 'movies', 'today'));
   }
 }
