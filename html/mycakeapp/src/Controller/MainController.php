d544146ee1ec9ed60409415ac0a753e3142c6e37
kazuyanakata <68310528+kazuyanakata@users.noreply.github.com>
2021-01-14 12:55:04 +0900

    add:上映スケジュール(VC)初回作成
    上映スケジュールページのview,controllerに用いるファイルを追加

---
+++ html/mycakeapp/src/Controller/MainController.php
@@ -0,0 +1,42 @@
+<?php
+
+namespace App\Controller;
+
+use App\Controller\AppController;
+use Cake\I18n\Time;
+use Cake\Event\Event;
+use Exception;
+
+class MainController extends AppController
+{
+  public $useTable = false;
+
+  public function initialize()
+  {
+    parent::initialize();
+    $this->loadModel('Movies');
+    $this->loadModel('Schedules');
+    $this->loadModel('Discounts');
+  }
+
+  public function schedule()
+  {
+    $week = ['日', '月', '火', '水', '木', '金', '土'];
+    $today = Time::now(); 
+    for ($i = 0; $i < 7; $i++) {
+      $date = Time::now();
+      $dates[$i] = $date->addDays($i);
+    }
+    $movies = $this->Movies->find()
+      ->where(['Movies.started_at <=' => $today])
+      ->andWhere(['Movies.finished_at >=' => $today])
+      ->andWhere(['Movies.is_deleted' => 0])
+      ->order(['Movies.started_at' => 'desc'])
+      ->contain('Schedules', function ($q) use ($today) {
+        return $q->where(['Schedules.start_date >=' => $today->format('Y-m-d 00:00:00')])
+        ->andWhere(['Schedules.start_date <=' => $today->format('Y-m-d 23:59:59')])
+        ->andWhere(['Schedules.is_deleted' => 0])->andWhere('Schedules.id IS NOT NULL');
+      });
+    $this->set(compact('dates', 'week', 'movies', 'today'));
+  }
+}
